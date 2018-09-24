d45f05331e0900409b932f025fc5d459ce2cc02e
Johnson Huynh <huynhjjk@gmail.com>
2018-09-24 00:26:44 -0700

    Working on cloud funcs

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,9 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.postDocument = functions.https.onRequest((req, res) => {
+    const requestBody = {
+        title: 'Testing Cloud Functions',
+        description: 'Hey',
+        category: 'Awesome',
+        date: 'Sept 24'
+    };
+    db.collection('items').add(requestBody);
+    // .then(docRef => {
+    //   return res.send(`Added ${docRef.id}`);
+    // })
+    // .catch(res.send(console.error));
+});
+exports.updateDocument = functions.https.onRequest((req, res) => {
+    const requestBody = {
+        title: 'Testing Update Cloud Functions!!!',
+    };
+    db.collection('items').doc('C5rhwJEbgcWsNBGIid94').update(requestBody);
+    // .then(docRef => {
+    //   return res.send(`Added ${docRef.id}`);
+    // })
+    // .catch(res.send(console.error));
+});
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+export const postDocument = functions.https.onRequest((req, res) => {
+  const requestBody = {
+    title: 'Testing Cloud Functions',
+    description: 'Hey',
+    category: 'Awesome',
+    date: 'Sept 24'    
+  }
+  db.collection('items').add(requestBody);
+  // .then(docRef => {
+  //   return res.send(`Added ${docRef.id}`);
+  // })
+  // .catch(res.send(console.error));
+});
+
+export const updateDocument = functions.https.onRequest((req, res) => {
+  const requestBody = {
+    title: 'Testing Update Cloud Functions!!!',
+  }
+  db.collection('items').doc('C5rhwJEbgcWsNBGIid94').update(requestBody);
+  // .then(docRef => {
+  //   return res.send(`Added ${docRef.id}`);
+  // })
+  // .catch(res.send(console.error));
+});
