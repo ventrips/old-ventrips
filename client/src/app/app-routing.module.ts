5cec644d57e1530e92fd05a6e1959c1f5461cc81
Johnson Huynh <huynhjjk@gmail.com>
2018-09-25 22:19:00 -0700

    Fixed default route path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
