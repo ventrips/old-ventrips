2a4a0c890c45adfd820f3dd009474295653983c6
Johnson Huynh <huynhjjk@gmail.com>
2018-09-27 23:47:50 -0700

    Creating hacker

---
+++ server/src/routes/hacker.ts
@@ -0,0 +1,45 @@
+import * as _ from 'lodash';
+import puppeteer from 'puppeteer';
+
+export class HackerNewsRoutes {
+
+    public routes(app: any): void {
+
+        app.route('/trending/hacker')
+        .get((req: any, res: any) => {
+            const url = 'https://news.ycombinator.com/news';
+            this.getTrendingHackerNews(req, res, url);
+        });
+    }
+
+    private getTrendingHackerNews(req: any, res: any, url: string): void {
+        (async function main() {
+            try {
+                const responseBody: any[] = [];
+                const browser = await puppeteer.launch({ headless: true });
+                const page = await browser.newPage();
+                // tslint:disable-next-line:max-line-length
+                page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+                await page.goto(url);
+                await page.waitForSelector('td.title');
+                const sections = await page.$$('.athing');
+                for (let i = 0; i < sections.length; i++) {
+                    const section = sections[i];
+                    const name = await section.$eval(
+                        '.title a',
+                        (item: any) => item.innerText.trim().replace(/\n/g, ' '),
+                    );
+                    const obj = {
+                        name,
+                    };
+                    responseBody.push(obj);
+                }
+                res.status(200).send(responseBody);
+            } catch (error) {
+                console.log(error);
+                res.status(500).send(error);
+            }
+        })();
+    }
+
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,20 +2,23 @@ import * as bodyParser from 'body-parser';
 import express from 'express';
 import * as utils from './lib/utils';
 import { GitHubRoutes } from './routes/github';
+import { HackerNewsRoutes } from './routes/hacker';
 import { InstagramRoutes } from './routes/instagram';
 import { YouTubeRoutes } from './routes/youtube';
 class App {
 
     public app: any;
 
+    public gitHubRoutes: GitHubRoutes = new GitHubRoutes();
+    public hackerNewsRoutes: HackerNewsRoutes = new HackerNewsRoutes();
     public instagramRoutes: InstagramRoutes = new InstagramRoutes();
     public youTubeRoutes: YouTubeRoutes = new YouTubeRoutes();
-    public gitHubRoutes: GitHubRoutes = new GitHubRoutes();
 
     constructor() {
         this.app = express();
         this.config();
         this.gitHubRoutes.routes(this.app);
+        this.hackerNewsRoutes.routes(this.app);
         this.instagramRoutes.routes(this.app);
         this.youTubeRoutes.routes(this.app);
         this.app.get('/', (req: any, res: any) => res.send('Hello World!'));
