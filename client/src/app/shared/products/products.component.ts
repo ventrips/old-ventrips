4013b22cc06ce30662f118f560ab368ab495a110
Johnson Huynh <huynhjjk@gmail.com>
2018-10-03 20:01:54 -0700

    Adding favicon and firebase

--- client/src/app/shared/products/products.component.ts
+++ client/src/app/shared/products/products.component.ts
@@ -1,53 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  public products = [
-    {
-      id: 1,
-      title: 'Threads',
-      category: 'Illustration',
-      image: './assets/img/500x500.svg'
-    },
-    {
-      id: 2,
-      title: 'Explore',
-      category: 'Graphic Design',
-      image: './assets/img/500x500.svg'
-    },
-    {
-      id: 3,
-      title: 'Finish',
-      category: 'Identity',
-      image: './assets/img/500x500.svg'
-    },
-    {
-      id: 4,
-      title: 'Lines',
-      category: 'Branding',
-      image: './assets/img/500x500.svg'
-    },
-    {
-      id: 5,
-      title: 'Southwest',
-      category: 'Website Design',
-      image: './assets/img/500x500.svg'
-    },
-    {
-      id: 6,
-      title: 'Window',
-      category: 'Photography',
-      image: './assets/img/500x500.svg'
-    }
-  ];
+  public products: Observable<any[]>;
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+    this.products = this.getProducts();
+  }
 
   ngOnInit(): void {
   }
 
+  documentToDomainObject = obj => {
+    const object = obj.payload.doc.data();
+    object.id = obj.payload.doc.id;
+    return object;
+  }
+
+  getProducts(): Observable<any[]> {
+    return this.db.collection('/products').snapshotChanges()
+    .pipe(map(actions => actions.map(this.documentToDomainObject)));
+  }
 }
