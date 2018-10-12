580507418956c6e5da3522a9fd1f973bde038a4f
Johnson Huynh <huynhjjk@gmail.com>
2018-10-12 06:04:05 -0700

    Adding some puppetteer code

--- client/functions/lib/index.js
+++ client/functions/lib/index.js
@@ -1,16 +1,43 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const request = require('request');
 const async = require('async');
 const Q = require('q');
 const cheerio = require('cheerio');
 const functions = require("firebase-functions");
+const puppeteer = require("puppeteer");
 const _ = require("lodash");
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
+exports.render = functions
+    .runWith({ memory: '1GB' })
+    // tslint:disable-next-line:no-shadowed-variable
+    .https.onRequest((request, response) => __awaiter(this, void 0, void 0, function* () {
+    // Launch a browser
+    const browser = yield puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    // Pass a URL via a query param
+    const requestURL = request.query.requestURL;
+    // Visit the page a get content
+    const page = yield browser.newPage();
+    yield page.goto(requestURL, { waitUntil: 'networkidle0' });
+    const content = yield page.content();
+    // Send the response
+    response.status(_.toNumber(status)).send(content);
+}));
 const getTrendingGitHubRepos = function () {
     const deferred = Q.defer();
     const baseUrl = 'https://github.com';
--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -3,6 +3,7 @@ const async = require('async');
 const Q = require('q');
 const cheerio = require('cheerio');
 import * as functions from 'firebase-functions';
+import * as puppeteer from 'puppeteer';
 import * as _ from 'lodash';
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -11,7 +12,33 @@ const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-const getTrendingGitHubRepos = function() {
+export const render = functions
+  .runWith({ memory: '1GB' })
+  // tslint:disable-next-line:no-shadowed-variable
+  .https.onRequest(async (request, response) => {
+
+    // Launch a browser
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
+    // Pass a URL via a query param
+    const requestURL = request.query.requestURL;
+
+    // Visit the page a get content
+    const page = await browser.newPage();
+
+    await page.goto(requestURL, { waitUntil: 'networkidle0' })
+
+    const content = await page.content();
+
+    // Send the response
+    response.status(_.toNumber(status)).send(content);
+  });
+
+
+  const getTrendingGitHubRepos = function() {
   const deferred = Q.defer();
   const baseUrl = 'https://github.com';
   const url = baseUrl + '/trending';
