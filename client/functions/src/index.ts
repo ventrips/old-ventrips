c579884e52617c8893fc2b8eea005d255edf46b1
Johnson Huynh <huynhjjk@gmail.com>
2018-10-17 19:54:48 -0700

    fixing

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -77,27 +77,9 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 const settings = { timestampsInSnapshots: true};
 db.settings(settings);
+
 const app = express();
 const main = express();
-// Add headers
-// main.use(function (req, res, next) {
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:13214');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-//     // Pass to next layer of middleware
-//     next();
-// });
 main.use('/api', app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
