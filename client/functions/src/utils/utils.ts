8f2d1fa3e0be531cfa7292327f4b22d18ac14e07
Johnson Huynh <huynhjjk@gmail.com>
2018-10-17 19:47:15 -0700

    Delete batch

--- client/functions/src/utils/utils.ts
+++ client/functions/src/utils/utils.ts
@@ -3,9 +3,27 @@ import * as cors from 'cors';
 const corsHandler = cors({origin: true});
 
 export function postBatchDocuments(db: any, data: object[], collectionName: string, req, res) {
+  const collectionRef = db.collection(collectionName);
+
+  // Delete Existing Data
+  collectionRef.get()
+  .then(querySnapshot => {
+    querySnapshot.forEach((doc) => {
+      doc.ref.delete().then(() => {
+        console.log("Document successfully deleted!");
+      }).catch(function(error) {
+        console.error("Error removing document: ", error);
+      });
+    });
+  })
+  .catch(function(error) {
+    console.log("Error getting documents: ", error);
+  });
+
   const writeBatch = db.batch();
+  // Add New Data
   _.forEach(data, item => {
-    const documentRef = db.collection(collectionName).doc();
+    const documentRef = collectionRef.doc();
     writeBatch.create(documentRef, item);
   });
   writeBatch.commit().then(() => {
