b94c1571ca0824a07719e178853383eae8099e26
Johnson Huynh <huynhjjk@gmail.com>
2018-11-17 10:46:31 -0800

    videography section and featured works

--- client/src/app/pages/portfolio/portfolio.component.ts
+++ client/src/app/pages/portfolio/portfolio.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { SeoService } from '../../services/seo/seo.service';
-
+import { DomSanitizer, SafeUrl, SafeResourceUrl, SafeScript } from '@angular/platform-browser';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  public youTubeUrls: Array<SafeResourceUrl>;
 
   constructor(
-    private seoService: SeoService
+    private seoService: SeoService,
+    private domSanitizer: DomSanitizer
   ) {
     this.seoService.generateTags();
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    const youTubeVideos = [
+      'Yd61DtkEXzM',
+      'ceTBRll9GLo',
+      'ZCDY0SmSIrI',
+      'QTcfoTJEFOE'
+    ];
+    this.youTubeUrls = this.getYouTubeUrls(youTubeVideos);
   }
 
+  sanitizeUrl(url: string): SafeResourceUrl {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  getYouTubeUrls(youTubeUrls: Array<string>): Array<SafeResourceUrl> {
+    return _.map(youTubeUrls, (youTubeUrl) => this.sanitizeUrl(`${'https://www.youtube.com/embed/'}${youTubeUrl}`));
+  }
 }
