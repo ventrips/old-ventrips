7d515f0cb74f6c7bfc4a130f47126f4d9c52a3ca
Johnson Huynh <huynhjjk@gmail.com>
2018-11-19 18:28:58 -0800

    coding icon colors

--- client/src/app/pages/portfolio/portfolio.component.ts
+++ client/src/app/pages/portfolio/portfolio.component.ts
@@ -9,39 +9,40 @@ import * as _ from 'lodash';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  public _ = _;
   public selected = false;
   public environment = environment;
   public portfolioWorks = [
     {
       title: 'Data 360',
       description: 'Migrated and rebuilt entire existing AngularJS web application to the latest Angular for Data 360 Team',
-      languages: ['HTML', 'CSS', 'JS']
+      languages: ['html5', 'css3', 'js', 'angular', 'node', 'npm', 'git']
     },
     {
       title: 'AskGET',
       description: 'Built an internal software store using the latest Angular 6 & Bootstrap 4 for AskGET Portal',
-      languages: ['HTML', 'CSS', 'JS']
+      languages: ['html5', 'css3', 'js', 'angular', 'node', 'npm', 'git']
     },
     {
       title: 'AIC Ozone',
       description: 'Built and redesigned internal cloud web application for AIC Ozone',
-      languages: ['HTML', 'CSS', 'JS']
+      languages: ['html5', 'css3', 'js', 'angular', 'node', 'npm', 'git']
     },
     {
       title: 'Order Capture Express',
       description: 'Redesigned the interface and improved the usability of the Order Capture Express application',
-      languages: ['HTML', 'CSS', 'JS']
-    },
-    {
-      title: 'Business Solutions',
-      description: 'Helped improve page load times and performance on www.att.com',
-      languages: ['HTML', 'CSS', 'JS']
+      languages: ['html5', 'css3', 'js', 'angular', 'node', 'npm', 'git']
     },
     {
       title: 'TripTips',
       description: `Designed the front-end and built a fully responsive web
       application using the MEAN Stack`,
-      languages: ['HTML', 'CSS', 'JS']
+      languages: ['html5', 'css3', 'js', 'angular', 'node', 'npm', 'git']
+    },
+    {
+      title: 'Business Solutions',
+      description: 'Helped improve page load times and performance on www.att.com',
+      languages: ['html5', 'css3', 'js']
     }
   ];
   public youTubeChannelUrl = 'https://www.youtube.com/channel/UCtif_sUZk9_F7CUzP3rPf2w';
